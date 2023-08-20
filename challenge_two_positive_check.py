b6473e13eeac9f4794888cd13d74b568a8e1914b
Eugene Aduogo <eugene.aduogo@student.moringaschool.com>
2023-08-20 12:16:30 +0300

    Updated condition check to match requirement

--- challenge_two_positive_check.py
+++ challenge_two_positive_check.py
@@ -3,4 +3,10 @@ def positive_check(a, b, c):
     pos = pos + 1 if a > 0 else pos
     pos = pos + 1 if b > 0 else pos
     pos = pos + 1 if c > 0 else pos
-    return pos > 1
+    return pos == 2
+
+# print(positive_check(2, 4, -3))
+# print(positive_check(-4, 6, 0))
+# print(positive_check(-14, -3, -4))
+# print(positive_check(4, 6, 10))
+# print(positive_check(4, -6, 9))
